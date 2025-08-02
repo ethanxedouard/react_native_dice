644d0f90bea14dd02eb49bed49f8ab570ca4ccff
ethanxedouard <edouardethan@gmail.com>
2025-08-01 17:44:08 -0700

    added picture and title to sign in screen

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,15 +1,18 @@
-import {View, Text, ScrollView, Image} from 'react-native'
+import {View, Text, ScrollView, Image, ImageBackground} from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import { Assets } from '@react-navigation/elements'
 
 const SignIn = () => {
     return (
-        <SafeAreaView className='bg-primary h-full'>
-            <ScrollView contentContainerClassName='h-full'>
-                <Image source={require("../assets/icons/dice.png")} />
-                
-            </ScrollView>
+        <SafeAreaView className="bg-accent-300 h-full">
+                <ScrollView contentContainerClassName='h-full'>  
+                    <Image source={require("../assets/icons/DiceLogo.png")} className="w-full h-3/6 my-17" resizeMode="contain" />
+                    <View className='px-10'>
+                        <Text className='justify-center text-6xl text-center uppercase font-pressStart2P-Regular text-primary-300 my'>Pixel</Text>
+                        <Text className='justify-center text-5xl text-center uppercase font-pressStart2P-Regular text-secondary-300'>Dice</Text>
+                    </View>
+                </ScrollView>
         </SafeAreaView>
     )
 }
